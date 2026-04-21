ea9dd41370586f4a1fa056c7f918b95436743298
Eve <evenlybigman@gmail.com>
2026-04-21 19:17:36 +0900

    n의 x승 함수

---
+++ CodePractice/CodePractice/power.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+double power(x, n) {      
+	if (n == 0) return 1;
+	else if (n % 2 == 0) return power(x * x, n / 2);
+	else return x * power(x * x ,(n - 1) / 2);
+}
+
+int main() {
+	printf("%lf", power(2, 4));
+}
