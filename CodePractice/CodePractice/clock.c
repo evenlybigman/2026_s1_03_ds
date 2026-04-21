0faf13eba54f3ceb07f68530fbf12bae40da262e
Eve <evenlybigman@gmail.com>
2026-04-21 18:17:01 +0900

    팩토리얼 함수

---
+++ CodePractice/CodePractice/clock.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main() {
+	clock_t start, stop;
+	double duration;
+	long long sum = 0;
+	start = clock();
+	for (long long i = 0; i < 1000000000; i++) {
+		sum += i;
+	}
+	stop = clock();
+	duration = (double)(stop - start) / CLOCKS_PER_SEC;
+	printf("sum의 값: %lld\n", sum);
+	printf("수행시간은 %lf초입니다.\n",duration);
+}
---
+++ CodePractice/CodePractice/time.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main() {
+	time_t start, stop;
+	double duration;
+	long long sum = 0;
+	start = time(NULL);
+	for (long long i = 0; i < 1000000000; i++) {
+		sum += i;
+	}
+	stop = time(NULL);
+	duration = (double)difftime(stop, start);
+	printf("sum의 값: %lld\n", sum);
+	printf("수행시간은 %lf초입니다.\n", duration);
+}
